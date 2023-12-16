1f964f56fb642e89a8719fe3959c07a1d3eb3b5b
Evgeny Liskevich <59793749+Barister@users.noreply.github.com>
2023-12-17 00:32:31 +0600

    All've done

--- src/App.js
+++ src/App.js
@@ -5,36 +5,30 @@ import { FacebookShareButton } from "react-share";
 const siteLink = "https://barister.github.io/quotes-machine/";
 const shortUrl = `https://shorturl.at/kwEFY`;
 
-function QuoteQuery({ quoteData, quoteLoaded }) {
-
+function QuoteQuery({ quoteData }) {
   return (
-
-    <CSSTransition in={quoteLoaded} appear={true} timeout={300} classNames='quote' mountOnEnter={true}>
+    <CSSTransition in={true} appear={true} timeout={300} classNames='quote' mountOnEnter={true}>
       <div>
         <i className="fa-solid fa-quote-left"></i>
         <p id='text'>{quoteData.quote}</p>
         <cite>- <span id='author'>{quoteData.author}</span></cite>
       </div>
     </CSSTransition>
-
   );
 }
 
-
-
-
 function QuoteRender({ quoteData, quoteLoaded }) {
+
   return (
-
     <blockquote className={`quote-box__content`}>
-      <QuoteQuery quoteData={quoteData} quoteLoaded={quoteLoaded} />
+      {quoteLoaded && <QuoteQuery quoteData={quoteData} />}
     </blockquote>
+  );
 
-  )
-};
+
+}
 
 function FacebookMediaButton({ quoteData }) {
-
   return (
     <FacebookShareButton
       url={siteLink}
@@ -42,20 +36,14 @@ function FacebookMediaButton({ quoteData }) {
       quote={`"${quoteData.quote}" - ${quoteData.author}`}
     >
       <a href='#' id='facebook-quote' target='_blank'>Facebook<i className="fa-brands fa-facebook" rel="noreferrer"></i></a>
-
     </FacebookShareButton>
   );
 }
 
 function TwitterShare({ quoteData }) {
-
   const quote = quoteData.quote;
   const tweetUnshortened = `"${quote}" - ${quoteData.author}`;
-
-
   const totalTweet = 280 - (tweetUnshortened.length + shortUrl.length + 11 + 4);
-
-  //console.log('tweetQuote:', tweetUnshortened, totalTweet);
 
   let tweetShortened = '';
 
@@ -66,10 +54,15 @@ function TwitterShare({ quoteData }) {
     tweetShortened = tweetUnshortened;
   }
 
-  //console.log('tweetShortened:', tweetShortened);
-
   return (
-    <a href={`https://twitter.com/intent/tweet?text=${tweetShortened}&hashtags=ourparents&url=${shortUrl}`} id='tweet-quote' target='_blank' rel="noreferrer">Twitter<i className="fa-brands fa-square-twitter"></i></a>
+    <a
+      href={`https://twitter.com/intent/tweet?text=${tweetShortened}&hashtags=ourparents&url=${shortUrl}`}
+      id='tweet-quote'
+      target='_blank'
+      rel='noreferrer'
+    >
+      Twitter<i className="fa-brands fa-square-twitter"></i>
+    </a>
   )
 }
 
@@ -84,36 +77,18 @@ export default function App() {
 
   const [quoteLoaded, setQuoteLoaded] = useState(false);
 
-
   const handleNextQuote = () => {
     fetchData();
   };
 
   const handlePreviousQuote = () => {
-    // console.log('Нужна предыдущая цитата!!!');
-
-    // console.log('предпоследний элемент массива:', quotesHistory[quotesHistory.length - 2]);
-
-    // console.log('последний элемент массива:', quotesHistory[quotesHistory.length - 1]);
     if (quotesHistory.length >= 2) {
-
       setQuoteData(quotesHistory[quotesHistory.length - 2]);
-      setQuoteLoaded(true);
-
-      // console.log('quotesHistory before pop:', quotesHistory);
-
-      quotesHistory = quotesHistory.slice(0, -2)
-
-      // console.log('quotesHistory after pop:', quotesHistory);
+      quotesHistory = quotesHistory.slice(0, -1);
     }
-
-
-
   };
 
   const fetchData = async () => {
-    setQuoteLoaded(false); // Устанавливаем флаг загрузки в false перед запросом
-
     try {
       const validCategories = ['dad', 'mom'];
       const randomCategory = validCategories[Math.floor(Math.random() * validCategories.length)];
@@ -132,54 +107,38 @@ export default function App() {
 
       const result = await response.json();
 
-
-
       setQuoteData({
         quote: result[0].quote,
         author: result[0].author
       });
+      setQuoteLoaded(true);
+      quotesHistory.push(result[0]);
     } catch (error) {
       console.error('Error:', error);
       setQuoteData({
         quote: 'Error loading quote from server',
         author: 'Evgenii Liskevich'
       });
-    } finally {
-      setQuoteLoaded(true);
-      //console.log('quoteLoad = true') // Устанавливаем флаг загрузки в true после получения данных или в случае ошибки
     }
   };
 
   useEffect(() => {
-    fetchData(); // Загрузка данных при монтировании компонента
+    fetchData();
   }, []);
 
-  if (quoteLoaded) {
-
-
-
-    quotesHistory.push(quoteData);
-
-    // console.log(quotesHistory);
-
-
-    return (
-      <>
-        <QuoteRender quoteData={quoteData} setQuoteData={setQuoteData} quoteLoaded={quoteLoaded} />
-        <div className='quote-box__panel'>
-          <div className='quote-box__socials socials'>
-
-            <FacebookMediaButton quoteData={quoteData} />
-            <TwitterShare quoteData={quoteData} />
-
-          </div>
-          <div className='quote-box__buttons'>
-            <button className='btn' id='previous-quote' onClick={handlePreviousQuote}>Previous</button>
-            <button className='btn' id='new-quote' onClick={handleNextQuote}>New Quote</button>
-          </div>
-        </div >
-      </>
-    );
-  }
-
+  return (
+    <>
+      <QuoteRender quoteData={quoteData} quoteLoaded={quoteLoaded} />
+      <div className='quote-box__panel'>
+        <div className='quote-box__socials socials'>
+          <FacebookMediaButton quoteData={quoteData} />
+          <TwitterShare quoteData={quoteData} />
+        </div>
+        <div className='quote-box__buttons'>
+          <button className='btn' id='previous-quote' onClick={handlePreviousQuote}>Previous</button>
+          <button className='btn' id='new-quote' onClick={handleNextQuote}>New Quote</button>
+        </div>
+      </div>
+    </>
+  );
 }
